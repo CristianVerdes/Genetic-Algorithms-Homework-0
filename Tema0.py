e623a69e76da3281835cdcefad1597cc275edeef
AXON\Cristian Verdes <cristian.verdesh@gmail.com>
2016-12-17 18:24:03 +0200

    first commit

---
+++ Tema0.py
@@ -0,0 +1,59 @@
+import math
+import random
+import time
+
+def rast_function(parameters = []):
+    nr_parameters = len(parameters)
+    s = 0
+    for number in parameters:
+        s = s + (pow(number,2) - 10 * math.cos(2 * math.pi * number))
+    return 10 * nr_parameters + s
+
+def griew_function(parameters = []):
+    nr_parameters = len(parameters)
+    fr = 4000
+    s = 0
+    p = 1
+    for number in parameters:
+        s = s + pow(number,2)
+    for i in range(1,len(parameters)):
+        p = p * math.cos(parameters[i] / math.sqrt(i))
+
+    return s / fr - p + 1
+
+def rosen_function (parameters = []):
+    print(parameters)
+    s = 0
+    for i in range(len(parameters)-1):
+        x = parameters[i] * parameters[i] - parameters[i+1]
+        s = s + 100 * pow(x, 2) + pow(parameters[i]-1, 2)
+    return s
+
+def six_function (parameters = []):
+    for number in parameters:
+        return (4 - 2.1 * pow(number, 2) + float((number ** 4)/3)) * pow(number,2) + \
+               number * number + (-4 + 4 * pow(number, 2)) * pow(number, 2)
+
+
+def tema0 ():
+    n = int(input("Introduceti nr. de parametri: "))
+    a = float(input("Minim interval: "))
+    b = float(input("Maxim interval: "))
+    numar_rulari = 100000
+    lista_rezultate = []
+    start_time = time.time()
+
+    for repetitie in range(numar_rulari):
+        parameters = []
+        for number in range(n):
+            parameters.append(random.uniform(a,b))
+        lista_rezultate.append(six_function(parameters))
+        # Mai sus schimbam functia folosita.
+
+    print("Cea mai buna solutie: ", min(lista_rezultate))
+    print("Cea mai proasta solutie: ", max(lista_rezultate))
+    print("Timp rulare: ", time.time()-start_time)
+    print("media solutiilor: ", sum(lista_rezultate)/float(numar_rulari))
+
+tema0()
+
